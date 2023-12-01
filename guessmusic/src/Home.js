0953599ce7c44cdfd6e21fe0e4ddc3d45b507049
atakanhbk <atakanhbk@gmail.com>
2023-12-01 19:48:07 +0300

    Add some css

--- guessmusic/src/Home.js
+++ guessmusic/src/Home.js
@@ -1,15 +1,14 @@
 import React from "react";
-import Logo from './image/logo.png';
+import Logo from "./image/logo.png";
 
 export default function Home() {
-  
   return (
     <div id="home">
       <header>
         <div className="trigger_logo">
           <div className="logo">
             <a className="creative_link" href="/">
-              <img width={100} src={Logo}/>
+              <img width={100} src={Logo} />
             </a>
           </div>
         </div>
@@ -34,7 +33,16 @@ export default function Home() {
         </div>
         <div className="account">Log in / Sign in</div>
       </header>
-      <div className="container"></div>
+      <div className="container">
+        <h3 className="main_title fs-1 fw-bold">Choose A Category</h3>
+        <div className="slider">
+          <ul className="category_card_parent">
+            <li className="category_card"><div>Image</div></li>
+            <li className="category_card"><div>Image</div></li>
+            <li className="category_card"><div>Image</div></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ guessmusic/src/RoutesController.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import VideoPlayer from "./VideoPlayer";
+
+const RoutesController = () => {
+  return (
+    <div>
+      {/* Your route definitions */}
+   
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <RoutesController />
+    </Router>
+  );
+};
+
+export default App;
