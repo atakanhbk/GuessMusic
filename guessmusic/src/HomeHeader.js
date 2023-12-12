c7261f3dd6aceabb42144881ad80ddf1cb193e83
atakanhbk <atakanhbk@gmail.com>
2023-12-12 18:06:30 +0300

    Update

--- guessmusic/src/HomeHeader.js
+++ guessmusic/src/HomeHeader.js
@@ -4,49 +4,51 @@ import Menu from "./Menu";
 
 export default function HomeHeader() {
   return (
-    <header>
-      <div className="trigger_logo">
-        <div className="logo">
-          <a className="logoIcon" href="/">
-            <img width={250} src={Logo} />
+    <div>
+      <header>
+        <div className="trigger_logo">
+          <div className="logo">
+            <a className="logoIcon" href="/">
+              <img width={250} src={Logo} />
+            </a>
+          </div>
+        </div>
+        <div className="nav">
+          <ul className="nav-ul">
+            <li>
+              <a className="creative_link" href="/">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="creative_link" href="#">
+                Leader Board
+              </a>
+            </li>
+            <li>
+              <a className="creative_link" href="/game">
+                Play
+              </a>
+            </li>
+            <li>
+              <a className="creative_link" href="/game">
+                Contact
+              </a>
+            </li>
+            <li>
+              <a className="creative_link" href="/game">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+        <Menu />
+        <div className="account">
+          <a href="#">
+            <div className="account-button">Log in / Log out</div>
           </a>
         </div>
-      </div>
-      <div className="nav">
-        <ul className="nav-ul">
-          <li>
-            <a className="creative_link" href="/">
-              Home
-            </a>
-          </li>
-          <li>
-            <a className="creative_link" href="#">
-              Leader Board
-            </a>
-          </li>
-          <li>
-            <a className="creative_link" href="/game">
-              Play
-            </a>
-          </li>
-          <li>
-            <a className="creative_link" href="/game">
-              Contact
-            </a>
-          </li>
-          <li>
-            <a className="creative_link" href="/game">
-              About
-            </a>
-          </li>
-        </ul>
-      </div>
-      <Menu />
-      <div className="account">
-        <a href="#">
-          <div className="account-button">Log in / Log out</div>
-        </a>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 }
